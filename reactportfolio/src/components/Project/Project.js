3cade73b30e8bdca4800b6dfc8ecdd7ec78b951f
bennaayy <jimenezbenjamin338@gmail.com>
2020-09-23 22:36:14 -0700

    port

--- reactportfolio/src/components/Project/Project.js
+++ reactportfolio/src/components/Project/Project.js
@@ -9,7 +9,7 @@ const Project = props => {
     return(
         <div>
         <Media>
-          <Media left top href="#">
+          <Media left middle href="#">
             <Media object data-src={props.picture} alt="Generic placeholder image" />
           </Media>
           <Media body>
@@ -22,29 +22,6 @@ const Project = props => {
           <Button color="secondary" size="lg" href={props.repo} active>Github Repo</Button>
         </Media>
         
-        <Media className="mt-1">
-          <Media left middle href="#">
-            <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
-          </Media>
-          <Media body>
-            <Media heading>
-              Project 2
-            </Media>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </Media>
-        </Media>
-        
-        <Media className="mt-1">
-          <Media left bottom href="#">
-            <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
-          </Media>
-          <Media body>
-            <Media heading>
-              Project 3
-            </Media>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </Media>
-        </Media>
       </div>
     );  
     
--- reactportfolio/src/pages/Portfolio/Portfolio.js
+++ reactportfolio/src/pages/Portfolio/Portfolio.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Project from '../../components/Project'
 import { Container } from 'reactstrap';
-import bitewallet from '../..pictures/Bite-Wallet.png'
-
+import BiteWallet from '../../pictures/Bite-Wallet.png'
+import RanchHand from '../../pictures/RanchHand.png'
+import Notetaker from '../../pictures/Notetaker.png'
+import Password from '../../pictures/PasswordGenerator.png'
+import Planner from '../../pictures/Planner.png'
+import Weather from '../../pictures/Weather.png'
 
 
 const Portfolio = () => {
@@ -15,9 +19,51 @@ const Portfolio = () => {
             description={"A application that allows users to check whatever stock you are invested in and will return up to date accurate information, as well as local restaurant suggestions based off of how well your chosen stock is doing."}
             app={" https://shellynguyen98.github.io/Project-1/"}
             repo={" https://github.com/ShellyNguyen98/Project-1"}
-            picture={"bitewallet"}
+            picture={BiteWallet}
+            />
+
+            <Project
+            name={"Ranch Hand"}
+            description={"This app was made for small or large scale horse ranches or equestrian centers to help keep track of the care and health of as many horses as you have."}
+            app={" https://gentle-taiga-08855.herokuapp.com/index.html"}
+            repo={" https://github.com/ShellyNguyen98/Project02"}
+            picture={RanchHand}
+            />
+
+            <Project
+            name={"Note Taker App"}
+            description={"This application allows a user to take notes, which will locally save and allows you to delete notes."}
+            app={" https://rocky-wildwood-41565.herokuapp.com/"}
+            repo={" https://github.com/BenJ-46/WK6HW1.git"}
+            picture={Notetaker}
+            />
+
+            <Project
+            name={"Password Generator"}
+            description={"Password Generator application allows a user to randomly generate a password"}
+            app={" https://benj-46.github.io/Week2HW1/"}
+            repo={" https://github.com/BenJ-46/Week2HW1.git"}
+            picture={Password}
+            />
+
+            <Project
+            name={"Day Planner"}
+            description={"This Day Planner app lets a user add events into a calender-themed webpage based on time of day, which also gives user ability to save and delete."}
+            app={" https://benj-46.github.io/Wk3HW1/"}
+            repo={" https://github.com/BenJ-46/Wk3HW1"}
+            picture={Planner}
+            />
+
+            <Project
+            name={"Weather App"}
+            description={"This weather application allows a user to type in any city name and a weather API will retrieve the local up-to-date data."}
+            app={" https://benj-46.github.io/Wk3HW2/"}
+            repo={" https://github.com/BenJ-46/Wk3HW2"}
+            picture={Weather}
             />
         </Container>
         </>
     )
 }
+
+export default Portfolio
