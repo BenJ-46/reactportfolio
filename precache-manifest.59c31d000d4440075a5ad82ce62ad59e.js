d9477429e34687439dc3dc22aef68d2a08b4e6ee
bennaayy <jimenezbenjamin338@gmail.com>
2020-10-04 14:28:45 -0700

    Updates

---
+++ precache-manifest.59c31d000d4440075a5ad82ce62ad59e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dda692f0b68fcd5b1a172ec43b5ea2",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "c088b4b138b2cd6b951f",
+    "url": "/reactportfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c088b4b138b2cd6b951f",
+    "url": "/reactportfolio/static/js/2.f8a58b07.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.f8a58b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82348eaed2a587fda7e3",
+    "url": "/reactportfolio/static/js/main.00ab715a.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "5d85fb42c5ddff7dda631b1c83581fbc",
+    "url": "/reactportfolio/static/media/RanchHand.5d85fb42.png"
+  }
+]);
